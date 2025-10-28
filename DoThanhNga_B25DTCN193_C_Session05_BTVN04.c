e358bcdec96320acda2551c1d0862e5499a6be78
ĐỖ THANH NGA <29dothanhnga@gmail.com>
2025-10-28 20:08:50 +0700

    Refactor tax calculation with salary and tax rate

--- DoThanhNga_B25DTCN193_C_Session05_BTVN04.c
+++ DoThanhNga_B25DTCN193_C_Session05_BTVN04.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
 int main() {
     // Tinh so tien thue thu nhap phai dong
-    float thu_nhap, thue;
+    float salary, tax_rate,tax;
     printf("So tien: ");
-    scanf("%2f", &thu_nhap);
-    if(0 < thu_nhap && thu_nhap <= 5) {
-        thue = thu_nhap * (5.0/100);
-    } else if(thu_nhap > 5 && 0 < thu_nhap && thu_nhap <= 10) {
-        thue = thu_nhap * (10.0/100);
+    scanf("%2f", &salary);
+    if(0 < salary && salary <= 5) {
+        tax_rate = 5.0/100;
+    } else if(salary > 5 && 0 < salary && salary <= 10) {
+        tax_rate = 10.0/100;
     } else {
-        thue = thu_nhap * (15.0/100);
+        tax_rate = 15.0/100;
     }
-    printf("Thue thu nhap phai dong: %.2f\n", thue);
+    tax = salary * tax_rate;
+    printf("Thue thu nhap phai dong: %.2f\n", tax);
     return 0;
 }
